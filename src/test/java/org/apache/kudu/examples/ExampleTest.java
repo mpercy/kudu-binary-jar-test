f3abd3feee99942ab1d4dedfa496505d14917079
Mike Percy <mpercy@users.noreply.github.com>
2019-03-04 18:47:41 -0800

    Initial import from kudu examples repository

---
+++ src/test/java/org/apache/kudu/examples/ExampleTest.java
@@ -0,0 +1,38 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package org.apache.kudu.examples;
+
+import static org.junit.Assert.assertTrue;
+
+import org.apache.kudu.client.KuduException;
+import org.apache.kudu.test.KuduTestHarness;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ExampleTest {
+
+    @Rule
+    public KuduTestHarness harness = new KuduTestHarness();
+
+    @Test
+    public void testCreateExampleTable() throws KuduException {
+        String tableName = "test_create_example";
+        Example.createExampleTable(harness.getClient(), tableName);
+        assertTrue(harness.getClient().tableExists(tableName));
+    }
+}
